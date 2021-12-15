9c04bf7dfc8294d1aad630d9c20216615c478cc0
Marco <93041518+upsidedown93@users.noreply.github.com>
2021-12-15 11:43:48 +0100

    Commit esercizio11

---
+++ exercise-11.js
@@ -0,0 +1,19 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+
+// const log = function (value) {
+//   console.log(value);
+// }
+
+// log(sum(2, 5));
+
+const sum = (a, b) => {
+  return a + b;
+}
+
+const log = (value) => {
+  console.log(value);
+}
+
+log(sum(2, 5));
